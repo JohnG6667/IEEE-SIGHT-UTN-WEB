3975680d5bcc7ac033d6cc29ccbf7164ceb23dbc
Jhostyn <jhotyn0305@hotmail.com>
2023-03-01 20:21:11 -0500

    Redireccióon login v2

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index'])->name('index.index');
 
-/*
-Route::middleware([
+
+/*Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
@@ -26,4 +26,4 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
- */
+*/
