87cd290b79b3bcb10d987c65226499b1c249c951
JohnG6667 <jagordont@utn.edu.ec>
2023-03-05 11:55:31 -0500

    Función para pantalla de Equipo

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
 use Illuminate\Http\Request;
 
 class MembersController extends Controller
 {
     public function index()
     {
+        $members = Member::whereHas('user', function ($query){
+            $query->where('status', 1);
+        })->paginate(6);
 
-        return view('index.members');
+        return view('index.members' , compact('members'));
     }
 }
--- resources/views/index/members.blade.php
+++ resources/views/index/members.blade.php
@@ -1,119 +1,75 @@
 <x-app-layout>
-                <section class="visual">
-                    <div class="visual-inner visual-banner dark-overlay parallax" data-stellar-background-ratio="0.55">
-                        <div class="container">
-                            <div class="visual-text-large text-left visual-center">
-                                <h1 class="visual-title visual-sub-title">The A Team</h1>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit porro laudantium sequi. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                <div class="breadcrumb-block">
-                                    <ol class="breadcrumb">
-                                        <li class="breadcrumb-item"><a href="{{ route('index.index') }}"> Inicio </a></li>
-                                        <li class="breadcrumb-item"><a href="index.html"> Elements </a></li>
-                                        <li class="breadcrumb-item active"> Animations </li>
-                                    </ol>
-                                </div>
-                            </div>
-                        </div>
+    <section class="visual">
+        <div class="visual-inner visual-banner dark-overlay parallax" data-stellar-background-ratio="0.55">
+            <div class="container">
+                <div class="visual-text-large text-left visual-center">
+                    <h1 class="visual-title visual-sub-title">Miembros de IEEE Sight</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit porro laudantium sequi. Lorem ipsum
+                        dolor sit amet, consectetur adipisicing elit.</p>
+                    <div class="breadcrumb-block">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('index.index') }}"> Inicio </a></li>
+                            <li class="breadcrumb-item">Miembros</li>
+                        </ol>
                     </div>
-                </section>
-                <!--/visual/banner of the page -->
-                <!-- main content wrapper -->
-                <div class="content-wrapper">
-                    <section class="content-block">
-                        <div class="container">
-                            <div class="heading bottom-space text-center">
-                                <h2>Learn. <span>Acheive.</span> Admire</h2>
-                            </div>
-                            <div class="description text-center">
-                                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery. </p>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <figure class="team-box caption-fade-up top-l-space">
-                                        <div class="img-block">
-                                            <img src="img/team-06.jpg" alt="images description">
-                                        </div>
-                                        <figcaption class="team-des-v2">
-                                            <span class="sub">Founder / CEO</span>
-                                            <strong class="content-title name">Nicola Gorani</strong>
-                                            <p>Put all speaking her delicate recurred every possible. Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. </p>
-                                        </figcaption>
-                                    </figure>
-                                </div>
-                                <div class="col-md-4">
-                                    <figure class="team-box caption-fade-up top-l-space">
-                                        <div class="img-block">
-                                            <img src="img/team-05.jpg" alt="images description">
-                                        </div>
-                                        <figcaption class="team-des-v2">
-                                            <span class="sub">Founder / CEO</span>
-                                            <strong class="content-title name">Jeremi Stralt</strong>
-                                            <p>Put all speaking her delicate recurred every possible. Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. </p>
-                                        </figcaption>
-                                    </figure>
-                                </div>
-                                <div class="col-md-4">
-                                    <figure class="team-box caption-fade-up top-l-space">
-                                        <div class="img-block">
-                                            <img src="img/team-04.jpg" alt="images description">
-                                        </div>
-                                        <figcaption class="team-des-v2">
-                                            <span class="sub">Web Programmer</span>
-                                            <strong class="content-title name">Colin Nizurb</strong>
-                                            <p>Put all speaking her delicate recurred every possible. Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. </p>
-                                        </figcaption>
-                                    </figure>
-                                </div>
-                                <div class="col-md-4">
-                                    <figure class="team-box caption-fade-up top-l-space">
-                                        <div class="img-block">
-                                            <img src="img/team-09.jpg" alt="images description">
-                                        </div>
-                                        <figcaption class="team-des-v2">
-                                            <span class="sub">Founder / CEO</span>
-                                            <strong class="content-title name">Darren Donovan</strong>
-                                            <p>Put all speaking her delicate recurred every possible. Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. </p>
-                                        </figcaption>
-                                    </figure>
-                                </div>
-                                <div class="col-md-4">
-                                    <figure class="team-box caption-fade-up top-l-space">
-                                        <div class="img-block">
-                                            <img src="img/team-08.jpg" alt="images description">
-                                        </div>
-                                        <figcaption class="team-des-v2">
-                                            <span class="sub">Visual Design Guru</span>
-                                            <strong class="content-title name">Ina Brettan</strong>
-                                            <p>Put all speaking her delicate recurred every possible. Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. </p>
-                                        </figcaption>
-                                    </figure>
-                                </div>
-                                <div class="col-md-4">
-                                    <figure class="team-box caption-fade-up top-l-space">
-                                        <div class="img-block">
-                                            <img src="img/team-07.jpg" alt="images description">
-                                        </div>
-                                        <figcaption class="team-des-v2">
-                                            <span class="sub">Web Programmer</span>
-                                            <strong class="content-title name">Leona Mattur</strong>
-                                            <p>Put all speaking her delicate recurred every possible. Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. </p>
-                                        </figcaption>
-                                    </figure>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <section class="content-block quotation-block black-overlay-6 parallax" data-stellar-background-ratio="0.55">
-                        <div class="container">
-                            <div class="inner-wrapper">
-                                <h3 class="block-top-heading text-white">BEST EVER DESIGN</h3>
-                                <h2 class="text-white">Thinking of joing a winning team?</h2>
-                                <div class="btn-container">
-                                    <a href="#" class="btn btn-primary">VIEW VACANCIES</a>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
                 </div>
+            </div>
+        </div>
+    </section>
+    <!--/visual/banner of the page -->
+    <!-- main content wrapper -->
+    <div class="content-wrapper">
+        <section class="content-block">
+            <div class="container">
+                <div class="heading bottom-space text-center">
+                    <h2>Learn. <span>Acheive.</span> Admire</h2>
+                </div>
+                <div class="description text-center">
+                    <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety
+                        immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel.
+                        Fat make met can must form into gate. Me we offending prevailed discovery. </p>
+                </div>
+                <div class="row">
+                    @if ($members->count())
+                        @foreach ($members as $member)
+                            <div class="col-md-4">
+                                <figure class="team-box caption-fade-up top-l-space">
+                                    <div class="img-block">
+                                        @if ($member->user->image)
+                                            <img src="{{Storage::url($member->user->image->url)}}" alt="images description">
+                                        @else
+                                            <img src="img/team-06.jpg" alt="images description">
+                                        @endif
+                                    </div>
+                                    <figcaption class="team-des-v2">
+                                        <span class="sub">Founder / CEO</span>
+                                        <strong class="content-title name">{{ $member->user->name }}
+                                            {{ $member->user->last_name }}</strong>
+                                        <p>{{ $member->description }}</p>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
+                <div class="btn-container full-width-btn top-space pagination justify-content-center">
+
+                    {{ $members->links() }}
+
+                </div>
+            </div>
+        </section>
+        <section class="content-block quotation-block black-overlay-6 parallax" data-stellar-background-ratio="0.55">
+            <div class="container">
+                <div class="inner-wrapper">
+                    <h3 class="block-top-heading text-white">BEST EVER DESIGN</h3>
+                    <h2 class="text-white">Thinking of joing a winning team?</h2>
+                    <div class="btn-container">
+                        <a href="#" class="btn btn-primary">VIEW VACANCIES</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 
 </x-app-layout>
